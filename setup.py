1aebb55a1a7f112e0b88bd7ef78fc94aa4c18f78
Steven Normore <snormore@gmail.com>
2014-03-26 20:13:19 -0400

    Rename referrer to pyreferrer.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='referrer',
+setup(name='pyreferrer',
       version='0.1',
       description='A referrer parser for Python.',
       url='http://github.com/snormore/pyreferrer',
       author='Steven Normore',
       author_email='snormore@gmail.com',
       license='MIT',
-      packages=['referrer'],
+      packages=['pyreferrer'],
       zip_safe=False)
