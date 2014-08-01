eba6d8336c25e11d5c8a847550e00dbbf2e509e7
Steven Normore <snormore@gmail.com>
2014-07-31 22:50:32 -0400

    fix setup.py included package data

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='pyreferrer',
-      version='0.2.3',
+      version='0.2.4',
       description='A referrer parser for Python.',
       url='http://github.com/snormore/pyreferrer',
       author='Steven Normore',
       author_email='snormore@gmail.com',
       license='MIT',
       packages=['pyreferrer'],
-      package_data={'pyreferrer': ['data/referrers.json', 'search.csv', ]},
+      package_data={'pyreferrer': ['data/*', ]},
       include_package_data=True,
       zip_safe=False)
