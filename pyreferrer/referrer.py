4c72effb7c04cc45315eb010a7d77cdeb1a88a5e
pushmatrix <daniel.beauchamp@gmail.com>
2014-08-21 02:44:44 -0400

    Refactor of pyreferrer. Now includes proper parsing of subdomains, tlds and domains.

--- pyreferrer/referrer.py
+++ pyreferrer/referrer.py
@@ -1,152 +1,75 @@
-from .rules import Rules
+from .ruleset import Ruleset
 import json
+import tldextract
 from urlparse import urlparse, parse_qs
 
 class Referrer:
 
   class Types:
+    UNKNOWN  = 'unknown'
     INDIRECT = 'indirect'
     DIRECT   = 'direct'
     SEARCH   = 'search'
     SOCIAL   = 'social'
     EMAIL    = 'email'
 
-  rules = Rules()
+  rules = Ruleset().rules
 
   @staticmethod
-  def parse_direct(raw_url, direct_domains, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    domain = url.netloc
-    if domain not in direct_domains:
-      return None
-    return {
-      'type': Referrer.Types.DIRECT,
-      'url': raw_url,
-      'domain': domain,
-    }
+  def parse_query_string(url, parameters):
+    if not parameters:
+      return ''
 
-  @staticmethod
-  def parse_email(raw_url, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    domain = url.netloc
-    if domain not in Referrer.rules.email:
-      return None
-    rule = Referrer.rules.email[domain]
-    return {
-      'type': Referrer.Types.EMAIL,
-      'url': raw_url,
-      'domain': rule['domain'],
-      'label': rule['label'],
-    }
-
-  @staticmethod
-  def parse_social(raw_url, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    domain = url.netloc
-    if domain not in Referrer.rules.social:
-      return None
-    rule = Referrer.rules.social[domain]
-    return {
-      'type': Referrer.Types.SOCIAL,
-      'url': raw_url,
-      'domain': rule['domain'],
-      'label': rule['label'],
-    }
-
-  @staticmethod
-  def parse_search_fuzzy(raw_url, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    domain = url.netloc
-    host_parts = domain.split('.')
-    for host_part in host_parts:
-      if host_part not in Referrer.rules.search_fuzzy:
-        continue
-      rule = Referrer.rules.search_fuzzy[host_part]
-      query_params = parse_qs(url.query, keep_blank_values=True)
-      query_common = set.intersection(set(query_params.keys()), set(rule['parameters']))
-      fragment_params = parse_qs(url.fragment, keep_blank_values=True)
-      fragment_common = set.intersection(set(fragment_params.keys()), set(rule['parameters']))
-      query = None
-      if len(query_common) > 0:
-        query = query_params[list(query_common)[0]][0]
-      elif len(fragment_common) > 0:
-        query = fragment_params[list(fragment_common)[0]][0]
-      elif '*' in rule['parameters']:
-        query = ''
-      if query is not None:
-        return {
-          'type': Referrer.Types.SEARCH,
-          'url': raw_url,
-          'domain': domain,
-          'label': rule['label'],
-          'query': query,
-        }
-    return None
-
-  @staticmethod
-  def parse_search(raw_url, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    domain = url.netloc
-    if domain not in Referrer.rules.search:
-      return Referrer.parse_search_fuzzy(raw_url, url=url)
-    rule = Referrer.rules.search[domain]
     query_params = parse_qs(url.query, keep_blank_values=True)
-    query_common = set.intersection(set(query_params.keys()), set(rule['parameters']))
+    query_common = set.intersection(set(query_params.keys()), set(parameters))
     fragment_params = parse_qs(url.fragment, keep_blank_values=True)
-    fragment_common = set.intersection(set(fragment_params.keys()), set(rule['parameters']))
+    fragment_common = set.intersection(set(fragment_params.keys()), set(parameters))
     query = ''
     if len(query_common) > 0:
       query = query_params[list(query_common)[0]][0]
     elif len(fragment_common) > 0:
       query = fragment_params[list(fragment_common)[0]][0]
-    elif '*' in rule['parameters']:
+    elif '*' in parameters:
       query = ''
-    else:
-      return Referrer.parse_search_fuzzy(raw_url, url=url)
-    return {
-      'type': Referrer.Types.SEARCH,
-      'url': raw_url,
-      'domain': rule['domain'],
-      'label': rule['label'],
-      'query': query,
-    }
+    return query
 
   @staticmethod
-  def parse_indirect(raw_url, url=None):
-    if not url:
-      url = urlparse(raw_url)
-    return {
+  def is_valid_url(url, domain_info):
+    return url.scheme and domain_info.domain and domain_info.tld
+
+  @staticmethod
+  def parse(raw_url, custom_rules=None):
+    rules = custom_rules or Referrer.rules
+    raw_url = raw_url.strip()
+    url = urlparse(raw_url)
+    domain_info = tldextract.extract(raw_url)
+
+    referrer = {
       'type': Referrer.Types.INDIRECT,
       'url': raw_url,
-      'domain': url.netloc,
+      'subdomain': domain_info.subdomain,
+      'domain': domain_info.domain,
+      'label': domain_info.domain.title(),
+      'tld': domain_info.suffix,
+      'path': url.path,
+      'query': ''
     }
 
-  @staticmethod
-  def parse(raw_url, direct_domains=[]):
-    url = urlparse(raw_url)
+    if Referrer.is_valid_url(url, domain_info):
+      # First check for an exact match of the url. Then check for a match with only the domain + tld
+      known_url = rules.get(url.netloc + url.path)\
+                  or rules.get(domain_info.registered_domain + url.path)\
+                  or rules.get(url.netloc)\
+                  or rules.get(domain_info.registered_domain)
 
-    referrer = Referrer.parse_direct(raw_url, direct_domains, url=url); 
-    if referrer:
-      return referrer
+      if known_url:
+        referrer['label'] = known_url['label']
+        referrer['type'] = known_url['type']
+        referrer['query'] = Referrer.parse_query_string(url, known_url.get('parameters'))
+    else:
+      referrer['type'] = Referrer.Types.UNKNOWN if raw_url else Referrer.Types.DIRECT
 
-    referrer = Referrer.parse_email(raw_url, url=url)
-    if referrer:
-      return referrer
+    return referrer
 
-    referrer = Referrer.parse_social(raw_url, url=url)
-    if referrer:
-      return referrer
-
-    referrer = Referrer.parse_search(raw_url, url=url)
-    if referrer:
-      return referrer
-
-    return Referrer.parse_indirect(raw_url, url=url)
-
-def parse(raw_url, direct_domains=[]):
-  return Referrer(raw_url, direct_domains).parse
+def parse(raw_url):
+  return Referrer(raw_url).parse
--- pyreferrer/referrer_test.py
+++ pyreferrer/referrer_test.py
@@ -2,32 +2,76 @@
 from referrer import *
 from nose.tools import assert_equals
 
-def test_relative_url():
-  referrer = Referrer.parse('buh')
+def test_parse_splits_the_url_into_its_components():
+  referrer = Referrer.parse('http://mysubdomain.myothersubdomain.supersite.co.uk/party/time?q=ohyeah&t=555')
   expected_referrer = {
     'type': Referrer.Types.INDIRECT,
-    'url': 'buh',
-    'domain': '',
+    'url': 'http://mysubdomain.myothersubdomain.supersite.co.uk/party/time?q=ohyeah&t=555',
+    'subdomain': 'mysubdomain.myothersubdomain',
+    'domain': 'supersite',
+    'label': 'Supersite',
+    'tld': 'co.uk',
+    'path': '/party/time',
+    'query': ''
   }
   assert_equals(expected_referrer, referrer)
 
-def test_direct_simple():
-  referrer = Referrer.parse('http://example.com', direct_domains=['example.com', 'sample.com'])
+def test_blank_referrer_is_classified_as_direct():
+  blank_referrer = Referrer.parse('')
+  whitespace_referrer = Referrer.parse('     ')
   expected_referrer = {
     'type': Referrer.Types.DIRECT,
-    'url': 'http://example.com',
-    'domain': 'example.com',
+    'url': '',
+    'subdomain': '',
+    'domain': '',
+    'label': '',
+    'tld': '',
+    'path': '',
+    'query': ''
   }
-  assert_equals(expected_referrer, referrer)
+  assert_equals(expected_referrer, blank_referrer)
+  assert_equals(expected_referrer, whitespace_referrer)
 
-def test_indirect_simple():
-  referrer = Referrer.parse('http://walrus.com/', direct_domains=['example.com', 'sample.com'])
-  expected_referrer = {
-    'type': Referrer.Types.INDIRECT,
-    'url': 'http://walrus.com/',
-    'domain': 'walrus.com',
-  }
-  assert_equals(expected_referrer, referrer)
+def test_a_url_that_does_not_match_any_known_urls_is_classified_as_indirect():
+  referrer = Referrer.parse('http://walrus.com/')
+  assert_equals(Referrer.Types.INDIRECT, referrer['type'])
+
+def test_a_url_that_does_not_match_any_known_urls_has_its_label_set_as_the_capitalized_domain():
+  referrer = Referrer.parse('http://walrus.com/')
+  assert_equals('Walrus', referrer['label'])
+
+def test_an_invalid_url_or_absolute_url_is_classified_as_unknown():
+  urls = [
+      'blap',
+      'blap blap',
+      'http://blapblap',
+      'http://.com',
+      'http://',
+      '/',
+    ]
+  for url in urls:
+    referrer = Referrer.parse(url)
+    assert_equals(referrer['type'], Referrer.Types.UNKNOWN)
+
+def test_parse_tries_to_match_a_known_url_using_everything_but_the_query_string():
+  rules = {"www.zambo.com/search": {"type": "search", "label": "Zambo"}}
+  referrer = Referrer.parse("http://www.zambo.com/search?q=hello!", rules)
+  assert_equals('search', referrer['type'])
+
+def test_parse_tries_to_match_a_known_url_using_the_domain_and_tld_and_path():
+  rules = {"zambo.com/search": {"type": "search", "label": "Zambo"}}
+  referrer = Referrer.parse("http://www.zambo.com/search?q=hello!", rules)
+  assert_equals('search', referrer['type'])
+
+def test_parse_tries_to_match_a_known_url_using_the_subdomains_domain_and_tld():
+  rules = {"www.zambo.com": {"type": "search", "label": "Zambo"}}
+  referrer = Referrer.parse("http://www.zambo.com/search?q=hello!", rules)
+  assert_equals('search', referrer['type'])
+
+def test_parse_tries_to_match_a_known_url_using_the_domain_and_tld():
+  rules = {"zambo.com": {"type": "search", "label": "Zambo"}}
+  referrer = Referrer.parse("http://www.zambo.com/search?q=hello!", rules)
+  assert_equals('search', referrer['type'])
 
 def test_email_simple():
   referrer = Referrer.parse('https://mail.google.com/9aifaufasodf8usafd')
@@ -35,7 +79,11 @@ def test_email_simple():
     'type': Referrer.Types.EMAIL,
     'label': 'Gmail',
     'url': 'https://mail.google.com/9aifaufasodf8usafd',
-    'domain': 'mail.google.com',
+    'subdomain': 'mail',
+    'domain': 'google',
+    'tld': 'com',
+    'path': '/9aifaufasodf8usafd',
+    'query': ''
   }
   assert_equals(expected_referrer, referrer)
 
@@ -45,49 +93,28 @@ def test_social_simple():
     'type': Referrer.Types.SOCIAL,
     'label': 'Twitter',
     'url': 'https://twitter.com/snormore/status/391149968360103936',
-    'domain': 'twitter.com',
+    'domain': 'twitter',
+    'subdomain': '',
+    'tld': 'com',
+    'path': '/snormore/status/391149968360103936',
+    'query': ''
   }
   assert_equals(expected_referrer, referrer)
 
-def test_social_with_prefix_www():
-  referrer = Referrer.parse('https://www.twitter.com/snormore/status/391149968360103936')
+def test_social_with_subdomain():
+  referrer = Referrer.parse('https://puppyanimalbarn.tumblr.com')
   expected_referrer = {
     'type': Referrer.Types.SOCIAL,
-    'label': 'Twitter',
-    'url': 'https://www.twitter.com/snormore/status/391149968360103936',
-    'domain': 'twitter.com',
+    'label': 'Tumblr',
+    'url': 'https://puppyanimalbarn.tumblr.com',
+    'domain': 'tumblr',
+    'subdomain': 'puppyanimalbarn',
+    'tld': 'com',
+    'path': '',
+    'query': ''
   }
   assert_equals(expected_referrer, referrer)
 
-def test_social_with_prefix_m():
-  referrer = Referrer.parse('https://m.twitter.com/snormore/status/391149968360103936')
-  expected_referrer = {
-    'type': Referrer.Types.SOCIAL,
-    'label': 'Twitter',
-    'url': 'https://m.twitter.com/snormore/status/391149968360103936',
-    'domain': 'twitter.com',
-  }
-  assert_equals(expected_referrer, referrer)
-
-def test_social_with_prefix_l():
-  referrer = Referrer.parse('https://l.twitter.com/snormore/status/391149968360103936')
-  expected_referrer = {
-    'type': Referrer.Types.SOCIAL,
-    'label': 'Twitter',
-    'url': 'https://l.twitter.com/snormore/status/391149968360103936',
-    'domain': 'twitter.com',
-  }
-  assert_equals(expected_referrer, referrer)
-
-def test_social_with_prefix_lm():
-  referrer = Referrer.parse('https://lm.twitter.com/snormore/status/391149968360103936')
-  expected_referrer = {
-    'type': Referrer.Types.SOCIAL,
-    'label': 'Twitter',
-    'url': 'https://lm.twitter.com/snormore/status/391149968360103936',
-    'domain': 'twitter.com',
-  }
-  assert_equals(expected_referrer, referrer)
 
 def test_social_google_plus():
   referrer = Referrer.parse('http://plus.url.google.com/url?sa=z&n=1394219098538&url=http%3A%2F%2Fjoe.blogspot.ca&usg=jo2tEVIcI5Wh-6t--v-1ODEeGG8.')
@@ -95,7 +122,11 @@ def test_social_google_plus():
     'type': Referrer.Types.SOCIAL,
     'label': 'Google+',
     'url': 'http://plus.url.google.com/url?sa=z&n=1394219098538&url=http%3A%2F%2Fjoe.blogspot.ca&usg=jo2tEVIcI5Wh-6t--v-1ODEeGG8.',
-    'domain': 'plus.url.google.com',
+    'domain': 'google',
+    'subdomain': 'plus.url',
+    'tld': 'com',
+    'path': '/url',
+    'query': ''
   }
   assert_equals(expected_referrer, referrer)
 
@@ -105,8 +136,11 @@ def test_search_simple():
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://search.yahoo.com/search?p=hello',
-    'domain': 'search.yahoo.com',
-    'query': 'hello',
+    'domain': 'yahoo',
+    'subdomain': 'search',
+    'tld': 'com',
+    'path': '/search',
+    'query': 'hello'
   }
   assert_equals(expected_referrer, referrer)
 
@@ -116,30 +150,39 @@ def test_search_with_query_in_fragment():
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://search.yahoo.com/search#p=hello',
-    'domain': 'search.yahoo.com',
-    'query': 'hello',
+    'domain': 'yahoo',
+    'subdomain': 'search',
+    'tld': 'com',
+    'path': '/search',
+    'query': 'hello'
   }
   assert_equals(expected_referrer, referrer)
 
-def test_search_with_fuzzy_yahoo_country():
+def test_search_with_yahoo_country():
   referrer = Referrer.parse('http://ca.search.yahoo.com/search?p=hello')
   expected_referrer = {
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://ca.search.yahoo.com/search?p=hello',
-    'domain': 'ca.search.yahoo.com',
-    'query': 'hello',
+    'domain': 'yahoo',
+    'subdomain': 'ca.search',
+    'tld': 'com',
+    'path': '/search',
+    'query': 'hello'
   }
   assert_equals(expected_referrer, referrer)
 
-def test_search_with_fuzzy_yahoo_country_and_query_in_fragment():
+def test_search_with_yahoo_country_and_query_in_fragment():
   referrer = Referrer.parse('http://ca.search.yahoo.com/search#p=hello')
   expected_referrer = {
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://ca.search.yahoo.com/search#p=hello',
-    'domain': 'ca.search.yahoo.com',
-    'query': 'hello',
+    'domain': 'yahoo',
+    'subdomain': 'ca.search',
+    'tld': 'com',
+    'path': '/search',
+    'query': 'hello'
   }
   assert_equals(expected_referrer, referrer)
 
@@ -149,8 +192,11 @@ def test_search_bing_not_live():
     'type': Referrer.Types.SEARCH,
     'label': 'Bing',
     'url': 'http://bing.com/?q=blargh',
-    'domain': 'bing.com',
-    'query': 'blargh',
+    'domain': 'bing',
+    'subdomain': '',
+    'tld': 'com',
+    'path': '/',
+    'query': 'blargh'
   }
   assert_equals(expected_referrer, referrer)
 
@@ -161,19 +207,25 @@ def test_search_non_ascii():
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://search.yahoo.com/search;_ylt=A0geu8fBeW5SqVEAZ2vrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDWmxUdFhVZ2V1eVVMYVp6c1VmRmRMUXUyMkxfbjJsSnVlY0VBQlhDWQRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANVRFRzSGFBUVF0ZUZHZ2hzZ0N3VDNBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgbXlzaG9waWZ5IHJlbmf4cmluZyBta29iZXRpYwR0X3N0bXADMTM4Mjk3MjM1NDIzMwR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+myshopify+rengøring+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1',
-    'domain': 'search.yahoo.com',
+    'domain': 'yahoo',
+    'subdomain': 'search',
+    'tld': 'com',
+    'path': '/search',
     'query': 'vinduespudsning myshopify rengøring mkobetic',
   }
   assert_equals(expected_referrer, referrer)
 
-def test_search_with_cryllics():
+def test_search_with_cyrillics():
   assert_equals.__self__.maxDiff = None
   referrer = Referrer.parse('http://www.yandex.com/yandsearch?text=%D0%B1%D0%BE%D1%82%D0%B8%D0%BD%D0%BA%D0%B8%20packer-shoes&lr=87&msid=22868.18811.1382712652.60127&noreask=1')
   expected_referrer = {
     'type': Referrer.Types.SEARCH,
     'label': 'Yandex',
     'url': 'http://www.yandex.com/yandsearch?text=%D0%B1%D0%BE%D1%82%D0%B8%D0%BD%D0%BA%D0%B8%20packer-shoes&lr=87&msid=22868.18811.1382712652.60127&noreask=1',
-    'domain': 'www.yandex.com',
+    'domain': 'yandex',
+    'subdomain': 'www',
+    'tld': 'com',
+    'path': '/yandsearch',
     'query': 'ботинки packer-shoes',
   }
   assert_equals(expected_referrer, referrer)
@@ -184,7 +236,10 @@ def test_search_with_explicit_plus():
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'http://search.yahoo.com/search;_ylt=A0geu8nVvm5StDIAIxHrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDSjNTOW9rZ2V1eVVMYVp6c1VmRmRMUkdDMkxfbjJsSnV2dFVBQmZyWgRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANDc01MSGlnTVFOS2k2cDRqcUxERzRBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgSk9LQVBPTEFSICIxMSArIDExIiBta29iZXRpYwR0X3N0bXADMTM4Mjk4OTYwMjg3OQR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+JOKAPOLAR+"11+%2B+11"+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1',
-    'domain': 'search.yahoo.com',
+    'domain': 'yahoo',
+    'subdomain': 'search',
+    'tld': 'com',
+    'path': '/search',
     'query': 'vinduespudsning JOKAPOLAR "11 + 11" mkobetic',
   }
   assert_equals(expected_referrer, referrer)
@@ -195,7 +250,10 @@ def test_search_with_empty_query():
     'type': Referrer.Types.SEARCH,
     'label': 'Yahoo!',
     'url': 'https://yahoo.com?p=&sa=t&rct=j&p=&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk',
-    'domain': 'yahoo.com',
+    'domain': 'yahoo',
+    'subdomain': '',
+    'tld': 'com',
+    'path': '',
     'query': '',
   }
   assert_equals(expected_referrer, referrer)
@@ -206,7 +264,10 @@ def test_search_google_https_with_no_params():
     'type': Referrer.Types.SEARCH,
     'label': 'Google',
     'url': 'https://google.com',
-    'domain': 'google.com',
+    'domain': 'google',
+    'subdomain': '',
+    'tld': 'com',
+    'path': '',
     'query': '',
   }
   assert_equals(expected_referrer, referrer)
@@ -217,7 +278,10 @@ def test_search_google_with_query():
     'type': Referrer.Types.SEARCH,
     'label': 'Google',
     'url': 'https://www.google.co.in/url?sa=t&rct=j&q=test&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk',
-    'domain': 'www.google.co.in',
+    'domain': 'google',
+    'subdomain': 'www',
+    'tld': 'co.in',
+    'path': '/url',
     'query': 'test',
   }
   assert_equals(expected_referrer, referrer)
