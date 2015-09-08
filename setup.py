e564fc8d9e91f66aa721ec54f9169f2e0dad3c9d
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-09-08 16:15:15 -0400

    Update setup.py
    updated version because we added more referrers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyreferrer',
-      version='0.3.2',
+      version='0.3.3',
       description='A referrer parser for Python.',
       url='http://github.com/Shopify/pyreferrer',
       author='Steven Normore',
