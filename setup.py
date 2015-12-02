9988547cf4cfdae3c7b209bec51d2c2f8972ec5f
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-12-02 11:07:33 -0500

    bumpy to 0.3.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyreferrer',
-      version='0.3.4',
+      version='0.3.5',
       description='A referrer parser for Python.',
       url='http://github.com/Shopify/pyreferrer',
       author='Steven Normore',
