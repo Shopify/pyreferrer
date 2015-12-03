ab69ad82ab54cb161649678e38e6a74a739909eb
Vanessa Sabino <vanessa.sabino@shopify.com>
2015-12-03 16:16:44 -0500

    update to version 0.3.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyreferrer',
-      version='0.3.5',
+      version='0.3.6',
       description='A referrer parser for Python.',
       url='http://github.com/Shopify/pyreferrer',
       author='Steven Normore',
