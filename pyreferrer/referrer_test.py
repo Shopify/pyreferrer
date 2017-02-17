3434c9b0c15282ad9790af9e27d16a28378ef995
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2017-02-17 11:01:40 -0500

    more python3

--- pyreferrer/referrer_test.py
+++ pyreferrer/referrer_test.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from pyreferrer.referrer import Referrer
 from nose.tools import assert_equals
 
--- pyreferrer/ruleset.py
+++ pyreferrer/ruleset.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from pkg_resources import resource_filename
+from io import open
 import json
 
 
