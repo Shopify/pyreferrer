92ee58fde6ee06063fea39487da5aac0cd421547
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2017-02-17 13:52:12 -0500

    bump so I can get it properly on pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyreferrer',
-      version='0.4.3',
+      version='0.4.4',
       description='A referrer parser for Python.',
       url='http://github.com/Shopify/pyreferrer',
       author='Shopify Data Acceleration',
