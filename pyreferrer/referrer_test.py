8e2337a85e1e4ce388ed84a4c774c3524e301029
Avantika <avantika.banerjee@github.com>
2019-10-22 14:57:21 +0200

    Update referrer_test.py

--- pyreferrer/referrer_test.py
+++ pyreferrer/referrer_test.py
@@ -154,7 +154,7 @@ def test_search_simple():
     referrer = Referrer.parse('http://search.yahoo.com/search?p=hello')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://search.yahoo.com/search?p=hello',
         'domain': 'yahoo',
         'subdomain': 'search',
@@ -169,7 +169,7 @@ def test_search_with_query_in_fragment():
     referrer = Referrer.parse('http://search.yahoo.com/search#p=hello')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://search.yahoo.com/search#p=hello',
         'domain': 'yahoo',
         'subdomain': 'search',
@@ -184,7 +184,7 @@ def test_search_with_yahoo_country():
     referrer = Referrer.parse('http://ca.search.yahoo.com/search?p=hello')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://ca.search.yahoo.com/search?p=hello',
         'domain': 'yahoo',
         'subdomain': 'ca.search',
@@ -199,7 +199,7 @@ def test_search_with_yahoo_country_and_query_in_fragment():
     referrer = Referrer.parse('http://ca.search.yahoo.com/search#p=hello')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://ca.search.yahoo.com/search#p=hello',
         'domain': 'yahoo',
         'subdomain': 'ca.search',
@@ -230,7 +230,7 @@ def test_search_non_ascii():
     referrer = Referrer.parse('http://search.yahoo.com/search;_ylt=A0geu8fBeW5SqVEAZ2vrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDWmxUdFhVZ2V1eVVMYVp6c1VmRmRMUXUyMkxfbjJsSnVlY0VBQlhDWQRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANVRFRzSGFBUVF0ZUZHZ2hzZ0N3VDNBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgbXlzaG9waWZ5IHJlbmf4cmluZyBta29iZXRpYwR0X3N0bXADMTM4Mjk3MjM1NDIzMwR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+myshopify+rengøring+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://search.yahoo.com/search;_ylt=A0geu8fBeW5SqVEAZ2vrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDWmxUdFhVZ2V1eVVMYVp6c1VmRmRMUXUyMkxfbjJsSnVlY0VBQlhDWQRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANVRFRzSGFBUVF0ZUZHZ2hzZ0N3VDNBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgbXlzaG9waWZ5IHJlbmf4cmluZyBta29iZXRpYwR0X3N0bXADMTM4Mjk3MjM1NDIzMwR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+myshopify+rengøring+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1',
         'domain': 'yahoo',
         'subdomain': 'search',
@@ -245,7 +245,7 @@ def test_search_with_explicit_plus():
     referrer = Referrer.parse('http://search.yahoo.com/search;_ylt=A0geu8nVvm5StDIAIxHrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDSjNTOW9rZ2V1eVVMYVp6c1VmRmRMUkdDMkxfbjJsSnV2dFVBQmZyWgRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANDc01MSGlnTVFOS2k2cDRqcUxERzRBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgSk9LQVBPTEFSICIxMSArIDExIiBta29iZXRpYwR0X3N0bXADMTM4Mjk4OTYwMjg3OQR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+JOKAPOLAR+"11+%2B+11"+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'http://search.yahoo.com/search;_ylt=A0geu8nVvm5StDIAIxHrFAx.;_ylc=X1MDMjExNDcyMTAwMwRfcgMyBGJjawMwbXFjc3RoOHYybjlkJTI2YiUzRDMlMjZzJTNEYWkEY3NyY3B2aWQDSjNTOW9rZ2V1eVVMYVp6c1VmRmRMUkdDMkxfbjJsSnV2dFVBQmZyWgRmcgN5ZnAtdC03MTUEZnIyA3NiLXRvcARncHJpZANDc01MSGlnTVFOS2k2cDRqcUxERzRBBG10ZXN0aWQDbnVsbARuX3JzbHQDMARuX3N1Z2cDMARvcmlnaW4DY2Euc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAM0NARxdWVyeQN2aW5kdWVzcHVkc25pbmcgSk9LQVBPTEFSICIxMSArIDExIiBta29iZXRpYwR0X3N0bXADMTM4Mjk4OTYwMjg3OQR2dGVzdGlkA01TWUNBQzE-?p=vinduespudsning+JOKAPOLAR+"11+%2B+11"+mkobetic&fr2=sb-top&fr=yfp-t-715&rd=r1',
         'domain': 'yahoo',
         'subdomain': 'search',
@@ -260,7 +260,7 @@ def test_search_with_empty_query():
     referrer = Referrer.parse('https://yahoo.com?p=&sa=t&rct=j&p=&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Yahoo!',
+        'label': 'Yahoo! Search',
         'url': 'https://yahoo.com?p=&sa=t&rct=j&p=&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk',
         'domain': 'yahoo',
         'subdomain': '',
@@ -275,7 +275,7 @@ def test_search_google_https_with_no_params():
     referrer = Referrer.parse('https://google.com')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Google',
+        'label': 'Google Search',
         'url': 'https://google.com',
         'domain': 'google',
         'subdomain': '',
@@ -290,7 +290,7 @@ def test_search_google_with_query():
     referrer = Referrer.parse('https://www.google.co.in/url?sa=t&rct=j&q=test&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Google',
+        'label': 'Google Search',
         'url': 'https://www.google.co.in/url?sa=t&rct=j&q=test&esrc=s&source=web&cd=1&ved=0CDkQFjAA&url=http%3A%2F%2Fwww.yellowfashion.in%2F&ei=aZCPUtXmLcGQrQepkIHACA&usg=AFQjCNE-R5-7CENi9oqYe4vG-0g0E7nCSQ&bvm=bv.56988011,d.bmk',
         'domain': 'google',
         'subdomain': 'www',
@@ -320,7 +320,7 @@ def test_search_google_adwords():
     referrer = Referrer.parse('http://www.google.ca/aclk?sa=l&ai=Cp3RJ8ri&sig=AOD64f7w&clui=0&rct=j&q=&ved=0CBoQDEA&adurl=http://www.domain.com/')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Google',
+        'label': 'Google Search',
         'url': 'http://www.google.ca/aclk?sa=l&ai=Cp3RJ8ri&sig=AOD64f7w&clui=0&rct=j&q=&ved=0CBoQDEA&adurl=http://www.domain.com/',
         'domain': 'google',
         'subdomain': 'www',
@@ -335,7 +335,7 @@ def test_search_google_pagead():
     referrer = Referrer.parse('http://www.googleadservices.com/pagead/aclk?sa=l&q=flowers&ohost=www.google.com')
     expected_referrer = {
         'type': Referrer.Types.SEARCH,
-        'label': 'Google',
+        'label': 'Google Ads',
         'url': 'http://www.googleadservices.com/pagead/aclk?sa=l&q=flowers&ohost=www.google.com',
         'domain': 'googleadservices',
         'subdomain': 'www',
